b8867693d26b85155ee53bb314f60fd33c56f7f5
lukechampine <luke.champine@gmail.com>
2024-04-12 00:19:30 -0400

    add urcrypt chacha bindings

---
+++ urcrypt/chacha.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <monocypher.h>
+
+void
+urcrypt_chacha_crypt(size_t rounds,
+                     uint8_t key[32],
+                     uint8_t nonce[8],
+                     uint64_t counter,
+                     size_t message_length,
+                     uint8_t *message)
+{
+  crypto_chacha_djb(message, message, message_length, rounds, key, nonce, counter);
+}
+
+void
+urcrypt_chacha_xchacha(uint8_t key[32],
+                       uint8_t nonce[24],
+                       uint8_t out_key[32],
+                       uint8_t out_nonce[8])
+{
+  crypto_chacha_h(out_key, key, nonce);
+  memcpy(out_nonce, nonce + 16, 8);
+}
